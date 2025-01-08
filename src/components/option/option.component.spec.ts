72b0e0d4b522b2501f21d13f04d43ec16d4f56fe
LevHecny <l.savin@yolka.tech>
2025-01-08 23:18:23 +0300

    specs

---
+++ src/components/option/option.component.spec.ts
@@ -0,0 +1,8 @@
+import {TestBed, ComponentFixture} from "@angular/core/testing";
+import {OptionComponent} from "./option.component";
+import {SelectionStore} from "../../store/selection.store";
+
+describe('OptionComponent', () => {
+    let component: OptionComponent;
+    let fixture: ComponentFixture<OptionComponent>;
+})
